ebb9e58ac37325c840f852fe25a36dc969ac3c70
cycankahuna <supreetvats55@gmail.com>
2023-09-17 12:48:12 +0530

    /

--- todolist/app/page.js
+++ todolist/app/page.js
@@ -1,5 +1,10 @@
 const page = () => {
   return;
-  <></>;
+
+  <>
+    <h1 className="bg-black text-white p-5 text-5xl font-bold text-center">
+      My To Do List
+    </h1>
+  </>;
 };
 export default page;
